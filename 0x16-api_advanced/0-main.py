faf137a4699e8849a481e78f40979db969e8d734
ila36IX <jbariali002@gmail.com>
2024-06-02 19:18:08 +0100

    Add the mudule doc

--- 0x16-api_advanced/0-main.py
+++ 0x16-api_advanced/0-main.py
@@ -1,7 +1,3 @@
-#!/usr/bin/python3
-"""
-0-main
-"""
 import sys
 
 if __name__ == '__main__':
--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,15 +8,9 @@ import requests
 def number_of_subscribers(subreddit):
     base_url = "https://www.reddit.com"
     endpoint = base_url + "/r/{}/about.json".format(subreddit)
-    subs = None
-    while subs is None:
-        try:
-            r = requests.get(endpoint, allow_redirects=False)
-            if r.status_code in (301, 302):
-                return 0
-            response = r.json().get('data')
-            subs = response.get('subscribers')
-        except Exception as e:
-            subs = None
-    subs = response.get('subscribers')
-    return subs
+    r = requests.get(endpoint, allow_redirects=False)
+    if r.status_code != 200:
+        return 0
+    response = r.json().get('data')
+    num_subs = response.get('subscribers')
+    return num_subs
