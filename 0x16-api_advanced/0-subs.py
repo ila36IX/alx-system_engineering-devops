8a59f168520a0498e3e56cf06079756b7c5c7cee
ila36IX <jbariali002@gmail.com>
2024-06-02 19:35:42 +0100

    Trying another solution

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,10 +7,11 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Number of subs"""
+    headers = {"User-Agent": "Chrome/125.0.0.0"}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     r = requests.get(url, allow_redirects=False)
     try:
         data = r.json()
-        return data['data']['subscribers'] 
-    except Exception as e:
+        return data['data']['subscribers']
+    except Exception:
         return 0
